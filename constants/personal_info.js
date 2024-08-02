9a861f70e2fbdfb041be000888766f0287729cf1
Tim Nguyen <timnguyen@tims-mbp.lan>
2024-08-02 15:30:25 -0700

    v1.0.1: temporarily adding ignored files to remote repository

---
+++ constants/personal_info.js
@@ -0,0 +1,7 @@
+// ENTER YOUR PERSONAL INFO HERE
+
+export const PERSONAL_INFO = {
+    CLIENT_ID: "{your_client_id_here}",
+    CLIENT_SECRET: "{your_client_secret_here}",
+    WEBSITE_URL: "{your_website_url_here}"
+}
